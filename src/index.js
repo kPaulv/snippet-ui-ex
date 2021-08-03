5ab23013c32346dcab112b60f6b61850f6aae670
Paul <pavel.k.313@mail.ru>
2021-08-03 23:00:32 +0300

    formatted index.js - comments deleted

--- src/index.js
+++ src/index.js
@@ -12,17 +12,6 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-/*ReactDOM.render(
-    <React.StrictMode>
-        <div className="PanelSpace">
-            <LeftPanel />
-            <FeedPanel />
-            <RightPanel />
-        </div>
-    </React.StrictMode>,
-    document.getElementById('root2')
-);*/
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
