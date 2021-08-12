c21fb9c2d052fecc17ff4936e2d9e629dff1140b
Paul <pavel.k.313@mail.ru>
2021-08-12 11:51:37 +0300

    changed SearchBar event to onSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,16 +12,22 @@ class SearchBar extends React.Component {
 
     render() {
         return (
+            <form className="search-bar-form" autoComplete={"off"} onSubmit={(e) => {
+                this.props.sendSearchTerm(this.state.searchTerm);
+                e.preventDefault();
+            }}>
             <input
                 type="text"
+                autoComplete={"new-password"}
                 id="search-bar-id"
                 className="search-bar"
                 placeholder="Search..."
-                onChange={(event) => {
+                onInput={(event) => {
                     this.setState({searchTerm: event.target.value});
-                    this.props.sendSearchTerm(event.target.value);
                 }}
             />
+            {/*<div style={{display: 'none'}}><button type={"submit"} /></div>*/}
+            </form>
         );
     }
 }
